a5c165434a799aff2bfe7491b957dd6de8869acb
Jake Snyder <jsnyder81@gmail.com>
2020-06-03 14:45:19 -0600

    fixed typo in ngrok wrapper

--- mist_receiver_config/ngrok_wrapper.py
+++ mist_receiver_config/ngrok_wrapper.py
@@ -5,5 +5,5 @@ def Get_NGROK_Tunnel(ip='ngrok', port=4040):
     header = {"content-type": "application/json"}
     response = requests.request("GET", 'http://ngrok:4040/api/tunnels', headers=header, data='')
     #response = requests.request("GET", 'http://localhost:4040/api/tunnels', headers=header, data='')
-    print(f"NGROK RESPONSE: {response.json()")
+    print(f"NGROK RESPONSE: {response.json()}")
     return response.json()['tunnels'][0]['public_url']
